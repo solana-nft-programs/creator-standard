82afaa7a03ef6b077993ffd121007e970b477bbc
Giannis Chatziveroglou <gchatz@mit.edu>
2022-10-19 17:46:48 -0700

    solita

---
+++ sdk/src/generated/index.ts
@@ -0,0 +1,17 @@
+import { PublicKey } from '@solana/web3.js'
+
+/**
+ * Program address
+ *
+ * @category constants
+ * @category generated
+ */
+export const PROGRAM_ADDRESS = 'mTok58Lg4YfcmwqyrDHpf7ogp599WRhzb6PxjaBqAxS'
+
+/**
+ * Program public key
+ *
+ * @category constants
+ * @category generated
+ */
+export const PROGRAM_ID = new PublicKey(PROGRAM_ADDRESS)
