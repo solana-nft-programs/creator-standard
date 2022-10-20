6887c66d16a871144e2704a8d6cdb4ac13efcbe5
Giannis Chatziveroglou <gchatz@mit.edu>
2022-10-20 15:48:18 -0700

    add mod.rs

--- program/src/instructions/mod.rs
+++ program/src/instructions/mod.rs
@@ -5,3 +5,14 @@ pub use mint_manager::update_mint_manager::*;
 pub mod standard;
 pub use standard::init_standard::*;
 pub use standard::update_standard::*;
+
+pub mod token;
+pub use token::approve;
+pub use token::burn;
+pub use token::close;
+pub use token::init_account;
+pub use token::init_mint;
+pub use token::post_transfer;
+pub use token::pre_transfer;
+pub use token::revoke;
+pub use token::transfer;
