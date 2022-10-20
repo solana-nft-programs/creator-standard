336e7ad0617c957ddec59493c26ba66484960294
jpbogle <jpbogle22@gmail.com>
2022-10-19 21:04:13 -0400

    Cleanup

---
+++ Makefile
@@ -0,0 +1,9 @@
+.PHONY: install test-keys build start test clean-test-keys stop
+
+TEST_KEY := $(shell solana-keygen pubkey ./tests/test-key.json)
+
+all: install test-keys build start test clean-test-keys stop
+
+build:
+	cd program && cargo build-bpf
+	cd sdk && yarn && yarn solita
--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,6 +1,8 @@
 use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey};
 solana_program::declare_id!("mTok58Lg4YfcmwqyrDHpf7ogp599WRhzb6PxjaBqAxS");
+
 #[cfg(not(feature = "no-entrypoint"))]
+
 solana_program::entrypoint!(process_instruction);
 pub fn process_instruction(
     _program_id: &Pubkey,
