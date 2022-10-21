63723bdc8755bf003a7575fdb62d47225f86780d
jpbogle <jpbogle22@gmail.com>
2022-10-21 02:45:11 -0400

    Rename disallowed addresses

--- program/src/instructions/standard/update_standard.rs
+++ program/src/instructions/standard/update_standard.rs
@@ -5,7 +5,7 @@ use anchor_lang::prelude::*;
 #[derive(AnchorSerialize, AnchorDeserialize)]
 pub struct UpdateStandardIx {
     pub check_seller_fee_basis_points: bool,
-    pub disallowed_programs: Vec<Pubkey>,
+    pub disallowed_addresses: Vec<Pubkey>,
     pub allowed_programs: Vec<Pubkey>,
 }
 
@@ -23,6 +23,6 @@ pub fn handler(ctx: Context<UpdateStandardCtx>, ix: UpdateStandardIx) -> Result<
     let standard = &mut ctx.accounts.standard;
     standard.check_seller_fee_basis_points = ix.check_seller_fee_basis_points;
     standard.allowed_programs = ix.allowed_programs;
-    standard.disallowed_programs = ix.disallowed_programs;
+    standard.disallowed_addresses = ix.disallowed_addresses;
     Ok(())
 }
