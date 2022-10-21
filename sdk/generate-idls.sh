76071113d51e0d8deea47c32a7feff10487b2243
Giannis Chatziveroglou <gchatz@mit.edu>
2022-10-21 16:46:41 -0700

    update generate-idls.sh

--- sdk/generate-idls.sh
+++ sdk/generate-idls.sh
@@ -2,13 +2,13 @@
 
 # This script generates the IDL JSONs without buildling the full packages.
 
-rm -rf src/idl/
-mkdir -p src/idl/
+rm -rf sdk/idl/
+mkdir -p sdk/idl/
 
 for PROGRAM in $(find ../program/ -maxdepth 3 -name lib.rs); do
     PROGRAM_NAME="cardinal_creator_standard"
     echo "Parsing IDL for $PROGRAM_NAME"
-    anchor idl parse --file $PROGRAM --out-ts src/idl/$PROGRAM_NAME.ts || {
+    anchor idl parse --file $PROGRAM --out-ts sdk/idl/$PROGRAM_NAME.ts || {
         echo "Could not parse IDL"
         exit 1
     }
