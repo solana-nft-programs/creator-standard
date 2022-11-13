2fcb41f07e11fc9aa57543f86a3906b915bb3cda
Giannis Chatziveroglou <gchatz@mit.edu>
2022-11-13 17:44:28 -0500

    createCSSToken script

--- tools/createCSSToken.ts
+++ tools/createCSSToken.ts
@@ -1,7 +1,14 @@
 import { findAta } from "@cardinal/common";
+import {
+  CreateMetadataV2,
+  Creator,
+  DataV2,
+  Metadata,
+} from "@metaplex-foundation/mpl-token-metadata";
 import * as anchor from "@project-serum/anchor";
-import { Keypair, PublicKey, Transaction } from "@solana/web3.js";
+import { Keypair, PublicKey } from "@solana/web3.js";
 import dotenv from "dotenv";
+import fetch from "node-fetch";
 
 import { createInitMintManagerInstruction, Ruleset } from "../sdk";
 import { findMintManagerId, findRulesetId } from "../sdk/pda";
@@ -10,36 +17,84 @@ import { connectionFor, createMintTx, executeTransaction } from "../utils";
 dotenv.config();
 
 const DEFAULT_RULESET = "ruleset-no-checks";
-const COLLECTOR = new PublicKey("gmdS6fDgVbeCCYwwvTPJRKM9bFbAgSZh6MTDUT2DcgV");
 
 export type CreateCSSTokenParams = {
-  target: PublicKey;
   rulesetName?: string;
+  target?: PublicKey;
 };
 
 const wallet = Keypair.fromSecretKey(
-  anchor.utils.bytes.bs58.decode(process.env.RULESET_AUTHORITY || "")
+  anchor.utils.bytes.bs58.decode(process.env.TEST_WALLET || "")
 ); // your wallet's secret key // your wallet's secret key
 
 const main = async (params: CreateCSSTokenParams, cluster = "devnet") => {
   const connection = connectionFor(cluster);
-  const transaction = new Transaction();
 
   const mintKeypair = Keypair.generate();
-  const tx = await createMintTx(
-    connection,
-    mintKeypair.publicKey,
-    wallet.publicKey
-  );
   const rulesetId = findRulesetId(params.rulesetName ?? DEFAULT_RULESET);
   const rulesetData = await Ruleset.fromAccountAddress(connection, rulesetId);
   const mintManagerId = findMintManagerId(mintKeypair.publicKey);
   const holdetAta = await findAta(
     mintKeypair.publicKey,
-    wallet.publicKey,
+    params.target || wallet.publicKey,
     true
   );
-  transaction.add(
+
+  // init mint
+  const tx = await createMintTx(
+    connection,
+    mintKeypair.publicKey,
+    wallet.publicKey,
+    params.target
+  );
+
+  // metadata
+  const metadataName = `TEST - ${new Date()
+    .toLocaleString("en-gb", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    })
+    .toUpperCase()}`;
+  const response = await fetch("https://picsum.photos/400", {
+    method: "GET",
+    redirect: "follow",
+  });
+  const metadataUri = `https://nft.cardinal.so/metadata?img=${
+    response.url
+  }&name=${encodeURIComponent(metadataName)}`;
+  const metadataId = await Metadata.getPDA(mintKeypair.publicKey);
+  tx.instructions.push(
+    ...new CreateMetadataV2(
+      { feePayer: wallet.publicKey },
+      {
+        metadata: metadataId,
+        metadataData: new DataV2({
+          name: metadataName,
+          symbol: "TST",
+          uri: metadataUri,
+          sellerFeeBasisPoints: 500,
+          creators: [
+            new Creator({
+              address: wallet.publicKey.toString(),
+              verified: false,
+              share: 100,
+            }),
+          ],
+          collection: null,
+          uses: null,
+        }),
+        updateAuthority: wallet.publicKey,
+        mint: mintKeypair.publicKey,
+        mintAuthority: wallet.publicKey,
+      }
+    ).instructions
+  );
+
+  // init mint manager
+  tx.add(
     createInitMintManagerInstruction({
       mintManager: mintManagerId,
       mint: mintKeypair.publicKey,
@@ -47,7 +102,7 @@ const main = async (params: CreateCSSTokenParams, cluster = "devnet") => {
       holderTokenAccount: holdetAta,
       tokenAuthority: wallet.publicKey,
       rulesetCollector: rulesetData.collector,
-      collector: COLLECTOR,
+      collector: rulesetData.collector,
       authority: wallet.publicKey,
       payer: wallet.publicKey,
     })
@@ -55,27 +110,19 @@ const main = async (params: CreateCSSTokenParams, cluster = "devnet") => {
 
   let txid = "";
   try {
-    txid = await executeTransaction(
-      connection,
-      transaction,
-      new anchor.Wallet(wallet)
+    txid = await executeTransaction(connection, tx, new anchor.Wallet(wallet), [
+      mintKeypair,
+    ]);
+    console.log(
+      `Created CCS token https://explorer.solana.com/tx/${txid}?cluster=${cluster}.`
     );
   } catch (e) {
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
     console.log(`Transactionn failed: ${e}`);
   }
-
-  try {
-    await Ruleset.fromAccountAddress(connection, rulesetId);
-    console.log(
-      `Initialized ruleset successfully https://explorer.solana.com/tx/${txid}?cluster=${cluster}.`
-    );
-  } catch (e) {
-    console.log("Could not initialize ruleset successfully.");
-  }
 };
 
 const params: CreateCSSTokenParams = {
-  target: new PublicKey(""),
+  target: new PublicKey("JBgna2pmtDJuUoAZ28j39LWWJTVGEoNUGDDGX87e58gC"),
 };
 main(params).catch((e) => console.log(e));
