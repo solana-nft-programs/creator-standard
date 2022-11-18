f35e79c6b717e4f39a7ad1201e161e4f15cf2479
Giannis Chatziveroglou <gchatz@mit.edu>
2022-11-18 15:00:44 -0800

    get rulest script

---
+++ tools/getRuleset.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+
+import { findRulesetId, Ruleset } from "../sdk";
+import { connectionFor } from "../utils";
+
+dotenv.config();
+
+export type RulesetParams = {
+  name: string;
+};
+
+const main = async (cluster = "devnet") => {
+  const connection = connectionFor(cluster);
+
+  const rulesetId = findRulesetId("ruleset-no-checks");
+  const ruleset = await Ruleset.fromAccountAddress(connection, rulesetId);
+  console.log(ruleset.name);
+  console.log(ruleset.allowedPrograms);
+};
+
+main().catch((e) => console.log(e));
